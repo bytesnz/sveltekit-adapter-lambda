d3f315c0a8db43540806be6182c59f286b888a8b
Jack Farley <or4e3dsw@bytes.nz>
2024-09-24 18:14:57 +1200

    Add DummyResolver

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import polka from "polka";
 import { ServerlessAdapter } from '@h4ad/serverless-adapter';
 import { AwsStreamHandler } from '@h4ad/serverless-adapter/handlers/aws';
 import { ApiGatewayV2Adapter } from '@h4ad/serverless-adapter/adapters/aws';
+import { DummyResolver } from '@h4ad/serverless-adapter/resolvers/dummy';
 import { PolkaFramework } from '@h4ad/serverless-adapter/frameworks/polka';
 
 const app = polka().use(internalHandler);
@@ -10,5 +11,6 @@ const app = polka().use(internalHandler);
 export const handler = ServerlessAdapter.new(app)
   .setFramework(new PolkaFramework())
   .setHandler(new AwsStreamHandler({ callbackWaitsForEmptyEventLoop: false }))
+  .setResolver(new DummyResolver())
   .addAdapter(new ApiGatewayV2Adapter())
   .build();
